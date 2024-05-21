814922d49bfc7a15ff424d16f9766e1d1266adf1
yjohnson67 <joh23004@byui.edu>
2024-05-21 16:18:06 -0500

    Adjustments to my site for event and weather.

---
+++ chamber/scripts/mode.js
@@ -0,0 +1,13 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🌔")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🌒";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🌔";
+	}
+});
