b9d9f7f7c5dce9f65ad65649f24ccf3faa539ab9
yjohnson67 <joh23004@byui.edu>
2024-06-01 22:44:18 -0500

    Finishing up week 6

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,6 +1,15 @@
-const temp= 57;
-const wSpeed= 15;
+const temp= 51
+const wSpeed= 4
+const nullstr= "Not Applicable";
 var windchill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
-
+ 
 var windchill= Math.round(windchill);
-document.getElementById("windchill").innerHTML= windchill;
+if ((temp<=50)&(wSpeed>=3)) {
+    document.getElementById("temp").innerHTML= "Temperture: " + temp + "&deg";
+    document.getElementById("wSpeed").innerHTML= "Wind Speed: " + wSpeed + "mph";
+    document.getElementById("windchill").innerHTML= "Wind Chill: " + windchill + "&deg";
+} else {
+    document.getElementById("temp").innerHTML= "Temperture: " + temp + "&deg";
+    document.getElementById("wSpeed").innerHTML= "Wind Speed: " + wSpeed + "mph";
+    document.getElementById("windchill").innerHTML= "Wind Chill: " + nullstr;
+}
