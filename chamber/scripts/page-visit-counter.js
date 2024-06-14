0e03e371707f5dd42ad90c6d033f0c261f03146c
yjohnson67 <joh23004@byui.edu>
2024-06-14 00:56:57 -0500

    Building the Prophet

--- chamber/scripts/page-visit-counter.js
+++ chamber/scripts/page-visit-counter.js
@@ -5,6 +5,7 @@ const visitsDisplay = document.querySelector(".visits");
 if (typeof(Storage) !== "undefined") {
     // Get the last visit date from localStorage
     var lastVisit = localStorage.getItem("lastVisit");
+    var numVisits = localStorage.getItem("numVisits")
 }
  
 //Declare Variables
@@ -12,13 +13,10 @@ const currentDate = new Date();
 const lastVisitDate = new Date(lastVisit);
 const timeDiff = currentDate - lastVisitDate;
 const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-
+ 
 // Message based on the time difference
 if (!lastVisit) {
     visitsDisplay.textContent = "Welcome! Let us know if you have any questions.";
-    if(!isNaN(lastVisitDate)){
-        const timeDiff = currentDate - lastVisitDate;
-    }
 } else if (daysDiff <= 1) {
     visitsDisplay.textContent = "Back so soon! Awesome!"
 } else {
@@ -26,4 +24,6 @@ if (!lastVisit) {
 }
  
 // Store the new date into localStorage, key=lastVisit
+numVisits++;
+localStorage.setItem("numVisits", numVisits);
 localStorage.setItem("lastVisit", currentDate);
--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,18 +1,46 @@
 //Declare Variables//
-const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json.";
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 const cards = document.querySelector('#cards');
-
-//async function to fetch the data fromt he JSON source url//
-async function getProphetData(){
-    //Store the response fromt he fetch() method ina const variable
-    const repsonse = await fetch(url);
-    //convert the response to a JSON object
-    const data = await Response.json();
-
-    //temporary testing of data retreival
-    console.table(data.prophets);
-}
-
-
-getProphetData();
-
+ 
+//async function to fetch data from the JSON source url//
+async function getProphetData(url) {
+    //Store the response from the fetch() method in a const variable
+    const response = await fetch(url);
+    //Convert the response to a JSON object
+    const data = await response.json(url);
+   
+    //Temporary testing of data retreival
+    //console.table(data.prophets);
+ 
+    //Reference the prophets array of the JSON data object, not just the object
+    displayProphets(data.prophets);
+  }
+ 
+  getProphetData(url);
+ 
+  //Define a function expression that handles a single parameter
+  const displayProphets = (prophets) => {
+    //Building cards for each prophet
+    prophets.forEach((prophet) => {
+      // Build div .cards elements
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+ 
+        // Build the h2
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+ 
+        //Build the img
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+ 
+        // Append the section(card)
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+ 
+        cards.appendChild(card);
+    });
+  }
