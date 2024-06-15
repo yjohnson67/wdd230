ed751729017555155ab1cc05e3b6293db44ea2ee
yjohnson67 <joh23004@byui.edu>
2024-06-14 23:45:00 -0500

    Pushing lessons

--- scripts/link.js
+++ scripts/link.js
@@ -6,7 +6,7 @@ const linksURL = "https://yjohnson67.github.io/wdd230/data/links.json";
 async function getLinks(linksURL) {
     const response = await fetch(linksURL);
     const data = await response.json(linksURL);
-    console.log(data.links);
+    console.log(data.lessons);
 }
 
 getLinks(linksURL);
