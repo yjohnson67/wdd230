cb0eee9e06104530809e2a3ea59fdade201388e0
yjohnson67 <joh23004@byui.edu>
2024-06-15 21:06:01 -0500

    trying to get my links to connect with my HTML week 09

--- scripts/link.js
+++ scripts/link.js
@@ -1,12 +1,34 @@
 //Declare Variables
+// your root wdd230 repository, GitHub pages URL.
 const baseURL = "https://yjohnson67.github.io/wdd230/";
-const linksURL = "https://yjohnson67.github.io/wdd230/data/links.json";
+const linksURL = "http://yjohnson67.github.io/wdd230/data/links.json";
+// const linksURL = "data/links.json";
 
-//async function to fetch data fromthe JSON
-async function getLinks(linksURL) {
-    const response = await fetch(linksURL);
-    const data = await response.json(linksURL);
-    console.log(data.lessons);
+
+// 4. Write an asynchronous function to get the links data in the links.json data file.
+// 5. Test the JSON result by writing the data to the console. In order to test your work 
+// at this point, you will need to write a line of code in links.js which calls the getLinks() 
+// function. Once your are satisfied or producing links, remove the console.log() statement.
+// 6. In the getLinks function, add a new line of code at the end that calls a function that 
+// will build out the available activity links from the data response. Name the function displayLinks.
+// Send the data as an argument.
+async function getLinks() {
+    try{
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+            displayLinks(data.weeks);
+        }
+        else {
+            throw Error(await response.text());
+        }        
+    }
+    catch (error) {
+        console.log(error);
+    }
+    
 }
 
-getLinks(linksURL);
+getLinks();
+
