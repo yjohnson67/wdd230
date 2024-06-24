a8ca88ab5f2af4bcd93f66d920983bf44c8f7d5d
yjohnson67 <joh23004@byui.edu>
2024-06-24 16:08:19 -0500

    Trying to get my announcement bar across my navagation.

---
+++ chamber/scripts/announcement.js
@@ -0,0 +1,26 @@
+// query selectors
+const announcement = document.querySelector('.announcement');
+var today = new Date();
+var day = today.getDay();
+ 
+ 
+// display banner only on Monday(1), Tuesday(2), and Wednesday (3)
+if (day === 1 || day === 2 || day === 3) {
+    // check local storage
+    if(localStorage.getItem('announcement') === 'hidden'){
+        // empty, do nothing    
+    } else {
+        const navHeight = document.querySelector('nav').offsetHeight;
+        //console.log(navHeight);
+        announcement.style.top = `${navHeight}px`;
+    }
+} else {
+    announcement.style.top = '-100px';
+}
+ 
+// handle closed announcement
+function hideAnnouncement() {
+    //console.log('Button Clicked');
+    announcement.style.top = '-100px';
+    localStorage.setItem('announcement', 'hidden');
+}
